8278700c540fd6c685f310553eb8aed6c71ddc5b
Jaideep Juneja <juneja0@purdue.edu>
2016-04-23 23:48:24 +0000

    trial

--- app.js
+++ app.js
@@ -21,6 +21,32 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+
+var pool = mysql.createConnection({
+connectionLimit: 10,
+host : 'us-cdbr-iron-east-03.cleardb.net',
+user : 'ba899d1ab08143',
+password : '799c47af',
+database : 'ad_54ae5c5533bec9b'	
+});
+
+pool.connect();
+
+app.get('/', function(req,res) {
+	
+	pool.query('SELECT * FROM ad_54ae5c5533bec9b.userpassscore',function(err,rows){
+		 if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+		
+		});
+		var resp = req.url;
+	res.send("Got a get request from server");
+	
+});
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
