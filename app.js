6ac568ec506df7b3261e91bd488e9fd2294ffae7
Shreyaansh Bassi <sbassi@purdue.edu>
2016-04-24 04:26:23 -0400

    REST Works!!!!!!!!!!

--- app.js
+++ app.js
@@ -6,14 +6,32 @@ var appEnv = cfenv.getAppEnv();
 
 var routes = require('./routes');
 var http = require('http');
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.set('port', (process.env.PORT || 6002));
 app.set('ip', (process.env.IP || "localhost"));
 
 //Default REST Call for Testing
-app.get('/hello', function(request, response) {
+app.post('/hello', function(req, res) {
 	//response.send('Hello World!');
-	console.log("call comes here");
+	console.log("It comes here!" + res.statusCode);
+	//console.log(request.body);
+	//response.send(request.body);
+   //console.log(response);
+	console.log(req.body.message);
+   //res.end(JSON.stringify(response));
+	res.send(req.body.message);
+});
+
+app.get('/hello', function(request, response) {
+	console.log("Hello!");
+	response.send("Hello World");
 });
 
 //Set the actual API Calls here!
--- public/js/test.js
+++ public/js/test.js
@@ -1,8 +1,25 @@
 function myFunction() {
-		  //document.getElementById("translateTo").value = "hrlp " + document.getElementById("translateFrom").value;
-		  var xhr = new XMLHttpRequest();
-		  xhr.open("GET", "http://localhost:6002/hello", false);
-		  xhr.send();
+	//document.getElementById("translateTo").value = document.getElementById("translateFrom").value;
+
+	//var url = "http://localhost:6002/hello";
+	/*var text = '{ "message":"' + document.getElementById(translateFrom) + '" }';
+	//var obj = JSON.parse(text);
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "plain/text");
+	var data = JSON.stringify({"email":"hey@mail.com","password":"101010"});
+	xhr.send("data");*/
+	xhr = new XMLHttpRequest();
+	var url = "http://localhost:6002/hello";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.addEventListener("readystatechange", function () {
+  		if (this.readyState === 4) {
+    		console.log(this.responseText);
+			document.getElementById("translateTo").value = this.responseText;
+  		}
+	});
+	var data = JSON.stringify({"message":document.getElementById("translateFrom").value});
+	xhr.send(data);
 }
-
-
