9441f53ff8165d4129960e0b44bb0e506b743986
Shreyaansh Bassi <sbassi@purdue.edu>
2016-04-23 23:58:50 -0400

    Changed the Ports, GET Request works in the Backend
    Yayay!!!

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ var appEnv = cfenv.getAppEnv();
 var routes = require('./routes');
 var http = require('http');
 
-app.listen(appEnv.port, '0.0.0.0', function() {
-  	console.log("server starting on " + appEnv.url);
+app.set('port', (process.env.PORT || 6002));
+app.set('ip', (process.env.IP || "localhost"));
+
+//Default REST Call for Testing
+app.get('/hello', function(request, response) {
+	response.send('Hello World!');
+});
+
+var server = app.listen(app.get('port'),app.get('ip'), function() {
+  	console.log("Server is listening on port "+app.get('port')+" and ip "+app.get('ip'));
 	
 	console.log("Hello Boiz!!! Printed on Screen");
 });
