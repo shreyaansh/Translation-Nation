afe69e270b63b8cee67c631f272fa2b0fd05aed7
Jaideep Juneja <juneja0@purdue.edu>
2016-04-25 09:17:31 +0000

    100% previous, yet to try

--- public/js/test.js
+++ public/js/test.js
@@ -176,7 +176,7 @@ function myFunction() {
 }
 function myFunction2()
 {
-	if(lang === "es")
+	/*if(lang === "es")
 			{
 			responsiveVoice.speak(document.getElementById("translateTo").value, "Spanish Female", {rate: 0.75});
   			}
@@ -191,5 +191,96 @@ function myFunction2()
   			else
   			{
 			responsiveVoice.speak(document.getElementById("translateTo").value, "Australian Female", {rate: 1});  				
-  			}
+  			}*/
+    
+    var e = document.getElementById("sel1");
+    var strlang = e.options[e.selectedIndex].value;
+    console.log(strlang);
+    if(strlang == 1)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Welsh Male", {rate: 0.75});
+        }
+    else if(strlang == 2)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Deutsch Female", {rate: 0.75});
+        }
+     else if(strlang == 3)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Russian Female", {rate: 0.75});
+        }
+     else if(strlang == 4)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Thai Female", {rate: 0.75});
+        }
+     else if(strlang == 5)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "US English Female", {rate: 0.75});
+        }
+     else if(strlang == 6)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "UK English Female", {rate: 0.75});
+        }
+     else if(strlang == 7)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "French Female", {rate: 0.75});
+        }
+     else if(strlang == 8)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Australian Female", {rate: 0.75});
+        }
+     else if(strlang == 9)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Spanish Female", {rate: 0.75});
+        }
+     else if(strlang == 10)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Italian Female", {rate: 0.75});
+        }
+     else if(strlang == 11)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Hindi Female", {rate: 0.75});
+        }
+     else if(strlang == 12)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Romanian Male", {rate: 0.75});
+        }
+     else if(strlang == 13)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Arabic Male", {rate: 0.75});
+        }
+     else if(strlang == 14)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Tamil Male", {rate: 0.75});
+        }
+     else if(strlang == 15)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Turkish Female", {rate: 0.75});
+        }
+    else if(strlang == 16)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Greek Female", {rate: 0.75});
+        }
+    else if(strlang == 17)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Norwegian Female", {rate: 0.75});
+        }
+    else if(strlang == 18)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Japanese Female", {rate: 0.75});
+        }
+    else if(strlang == 19)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Korean Female", {rate: 0.75});
+        }
+    else if(strlang == 20)
+        {
+            responsiveVoice.speak(document.getElementById("translateTo").value, "Chinese Female", {rate: 0.75});
+        }
+    
+}
+function myFunction3()
+{
+    var e = document.getElementById("sel1");
+    var strlang = e.options[e.selectedIndex].text;
+    console.log(strlang);
 }
