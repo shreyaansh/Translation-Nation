166fc4100a0c2f1b939f620c9e012b5dfe8a1b26
Shreyaansh Bassi <shreyaanshbassi@gmail.com>
2016-04-26 07:29:20 +0000

    Please work

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 /*eslint-env node, browser*/
-/*globals toastr */
+/*globals toastr data*/
 function authenticate() {
 	console.log("It comes here!");
 	var data = JSON.stringify({"username": document.getElementById("usr").value, "password": document.getElementById("pwd").value});
@@ -31,7 +31,7 @@ function signUp() {
 	var data = JSON.stringify({"username": document.getElementById("usr").value, "password": document.getElementById("pwd").value});
 	var xhr = new XMLHttpRequest();
 
-	xhr.addEventListener("readystatechange", function ({
+	xhr.addEventListener("readystatechange", function () {
   		if (this.readyState === 4) {
 			console.log(this.responseText);
 			if (this.responseText === "success")
