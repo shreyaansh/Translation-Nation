9c419a2aeb3ff4167336a9a8dd1488bb7f202a96
Shreyaansh Bassi <shreyaanshbassi@gmail.com>
2016-04-26 15:11:47 +0000

    Previous changes

--- public/js/test.js
+++ public/js/test.js
@@ -147,5 +147,7 @@ function myFunction3() {
 }
 
 function hello() {
-	//localStorage.setItem('username', 'help');
+	var username = localStorage.getItem('username');
+	console.log(username);
+	document.getElementById('user').innerHTML = '<a href="#"><span class="glyphicon glyphicon-user"></span> ' + username + '</a>';
 }
